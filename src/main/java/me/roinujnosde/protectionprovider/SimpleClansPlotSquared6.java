38e5f64f2fa160036ae6358f1ef4d5249b128eeb
RoinujNosde <edsonpassosjr@outlook.com>
2022-06-23 19:23:11 -0300

    First commit

---
+++ src/main/java/me/roinujnosde/protectionprovider/SimpleClansPlotSquared6.java
@@ -0,0 +1,34 @@
+package me.roinujnosde.protectionprovider;
+
+import me.roinujnosde.protectionprovider.provider.PS6Provider;
+import net.sacredlabyrinth.phaed.simpleclans.SimpleClans;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class SimpleClansPlotSquared6 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getScheduler().runTask(this, this::registerProvider);
+    }
+
+    private void registerProvider() {
+        PluginManager pluginManager = getServer().getPluginManager();
+        if (pluginManager.getPlugin("PlotSquared") == null) {
+            getLogger().severe("PlotSquared not found! Disabling...");
+            pluginManager.disablePlugin(this);
+            return;
+        }
+        Plugin scPl = pluginManager.getPlugin("SimpleClans");
+        if (scPl == null) {
+            getLogger().severe("SimpleClans not found! Disabling...");
+            pluginManager.disablePlugin(this);
+            return;
+        }
+        getLogger().info("Registering provider...");
+        ((SimpleClans) scPl).getProtectionManager().registerProvider(new PS6Provider());
+    }
+}
